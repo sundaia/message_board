53842aea4383c0fcbe6b7198167fed889be62bfd
zardi <zardi@nakano31>
2023-07-02 01:36:14 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,36 +1,40 @@
 package models;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
-import com.google.protobuf.Timestamp;
-
+@Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
+@Table(name = "messages")
 public class Message {
     @Id
-    @Column(name="id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="title",length=225,nullable=false)
+    @Column(name = "title", length = 255, nullable = false)
     private String title;
 
-    @Column(name="content",length=225,nullable=false)
+    @Column(name = "content", length = 255, nullable = false)
     private String content;
 
-    public String getCreated_at() {
-        return created_at;
-    }
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
 
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
-
-    @Column(name="created_at",length=225,nullable=false)
-    private String created_at;
-
-    @Column(name="updated_at",nullable=false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
     public Integer getId() {
@@ -57,6 +61,14 @@ public class Message {
         this.content = content;
     }
 
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
     public Timestamp getUpdated_at() {
         return updated_at;
     }
@@ -64,6 +76,4 @@ public class Message {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
-
-
 }
